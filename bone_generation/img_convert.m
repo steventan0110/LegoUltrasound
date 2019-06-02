0e6849935f383c7ff441c8593126cfd39b4232dd
Steven Tan <wtan12@jhu.edu>
2019-06-02 15:14:29 -0400

    manually segmentation finished

--- bone_generation/img_convert.m
+++ bone_generation/img_convert.m
@@ -14,28 +14,33 @@ pout_adapthisteq = adapthisteq(img);
 %from comparison, the adapthisteq technique is the most useful one
 %Now find the binary mask
 image = pout_adapthisteq;
-imshow(image)
+imshow(image) 
 h1 = drawline('SelectedColor','yellow');
 h1.Selected = true;
-col = h1.Color;
-% h2 = drawline('SelectedColor','yellow');
-% h2.Selected = true;
-% h3 = drawline('SelectedColor','yellow');
-% h3.Selected = true;
-% h4 = drawline('SelectedColor','yellow');
-% h4.Selected = true;
-% h5 = drawline('SelectedColor','yellow');
-% h5.Selected = true;
-% h6 = drawline('SelectedColor','yellow');
-% h6.Selected = true;
-% h7 = drawline('SelectedColor','yellow');
-% h7.Selected = true;
-% h8 = drawline('SelectedColor','yellow');
-% h8.Selected = true;
-% h9 = drawline('SelectedColor','yellow');
-% h9.Selected = true;
-% h10 = drawline('SelectedColor','yellow');
-% h10.Selected = true;
+h2 = drawline('SelectedColor','yellow');
+h2.Selected = true;
+h3 = drawline('SelectedColor','yellow');
+h3.Selected = true;
+h4 = drawline('SelectedColor','yellow');
+h4.Selected = true;
+h5 = drawline('SelectedColor','yellow');
+h5.Selected = true;
+h6 = drawline('SelectedColor','yellow');
+h6.Selected = true;
+h7 = drawline('SelectedColor','yellow');
+h7.Selected = true;
+h8 = drawline('SelectedColor','yellow');
+h8.Selected = true;
+h9 = drawline('SelectedColor','yellow');
+h9.Selected = true;
+h10 = drawline('SelectedColor','yellow');
+h10.Selected = true;
+h11 = drawline('SelectedColor','yellow');
+h11.Selected = true;
+M = createMask(h1) | createMask(h2)|createMask(h3)|createMask(h4)|createMask(h5)...
+    |createMask(h6)|createMask(h7)|createMask(h8)|createMask(h9)|createMask(h10)...
+    |createMask(h11);
+imshow(M)
+save('mask', M)
 
-disp(h1)
 Bmask = image;
