6671acbec521513520d13a90776df71a0cb24cb9
Steven Tan <wtan12@jhu.edu>
2019-06-07 17:09:28 -0400

    add randomization

---
+++ bone_generation/rnd_bone.m
@@ -0,0 +1,7 @@
+%function that implement data augmentation technique to randomize the
+%boundary points of the bone mask to generate more medium for kwave
+function mask = rnd_bone(image, num)
+
+%utilize data augmentation:
+
+
